72919c071b323b39c2b5902ed68d17a93556dcb7
Dikkoo <58894349+DikkooXie@users.noreply.github.com>
2023-02-18 09:35:43 +0800

    Add files via upload

--- code/tablePrint.h
+++ code/tablePrint.h
@@ -1,8 +1,8 @@
 #pragma once
 
-// Â´Â°Â¿ÃšÂ¿Ã­Â¶Ãˆ
+// ´°¿Ú¿í¶È
 #define WIN_WIDTH 80
-// Â´Â°Â¿ÃšÂ¸ÃŸÂ¶Ãˆ
+// ´°¿Ú¸ß¶È
 #define WIN_HEIGHT 25
 
 #define TABLE_WIDTH 78
@@ -13,8 +13,8 @@
 void printTableHead(int tableWidth);
 void printTableTail(int tableWidth);
 void printTableRow(int tableWidth, char cols[][COL_LEN_MAX], int n);
-//void printTableHead(int tableWidth, int cols);
-//void printTableTail(int tableWidth, int cols);
-//void printTableMidLine(int tableWidth, int cols);
+void printTableHead(int tableWidth, int cols);
+void printTableTail(int tableWidth, int cols);
+void printTableMidLine(int tableWidth, int cols);
 void printTableMidInfo(int tableWidth, const char* str);
 void printMidInfo(const char* str);
